02d6881687ef8392acbff3de8fdfb80de95d4e0f
KingKornish <96269962+KingKornish@users.noreply.github.com>
2021-12-26 01:49:28 +0000

    Add files via upload

---
+++ Imperial_Metric.py
@@ -0,0 +1,12 @@
+def meter(foot):
+    return float(foot / 3.28)
+
+def kilometerm(mile):
+    return float(mile / 0.62)
+
+def kilometery(yard):
+    return float(yard / 1093.61)
+
+def centimeter(inch):
+    return float(inch / 0.39)
+    
---
+++ Modules_Practice.py
@@ -0,0 +1,11 @@
+import Imperial_metric.py
+
+def greet(name):
+    return f"\nHello {name}"
+
+name = input("Hi there, What is your name?\n")
+
+greet(name)
+
+Imperial_metric.centimeter(2) 
+print(f"{inch} inches is equal to " + str(centimeter) + "cm")
